371970fb2a27f4d09219c6d06f86b0628033a67d
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-07 16:20:27 +0530

    back-end first commit

---
+++ back-end/src/main/java/com/silverlineIt/courseupload/CourseuploadApplication.java
@@ -0,0 +1,13 @@
+package com.silverlineIt.courseupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseuploadApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/silverlineIt/courseupload/CourseuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silverlineIt.courseupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
