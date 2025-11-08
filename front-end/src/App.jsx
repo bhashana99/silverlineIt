e0c6b610d84f4d3e853af190f5ec237f8e311e6b
bhashana99 <bhashanachamodya99@gmail.com>
2025-11-08 16:26:14 +0530

    Impelement PrivateRoute for protect private route

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
+import PrivateRouter from "./components/PrivateRouter.jsx";
+import Dashboard from "./pages/Instructor/Dashboard.jsx";
 
 
 
@@ -13,16 +15,27 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRouter>
+              <Dashboard/>
+            </PrivateRouter>
+          }
+          
+        />
+
 
         </Routes>
-      
-      <Footer/>
+
+      <Footer />
 
     </div>
   )
